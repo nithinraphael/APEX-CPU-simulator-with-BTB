b5eb5eba7ca8f9c5c046e2dca7e9c52a21a7a309
Nithin Raphael <nithin.raphael96@gmail.com>
2023-09-29 18:21:26 -0400

    chore: add code to check progress

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "errors/index.hpp"
 #include "fs/index.hpp"
+#include "instruction/index.hpp"
+#include "instruction/instruction.hpp"
 #include "utils/index.hpp"
 
 #include <iostream>
+#include <ostream>
 
 using namespace fs;
 using namespace std;
 using namespace utils;
+using namespace instruction;
 
 int main(int argc, char* argv[])
 {
@@ -28,6 +32,25 @@ int main(int argc, char* argv[])
         {
             std::cout << str << std::endl;
         }
+    }
+
+    Result<Instruction, Error> inst = parseInstruction("SUB R3, R5");
+
+    cout << "Opcode: " << inst.getValue().opcode << endl;
+    if (inst.getValue().operand1)
+    {
+
+        cout << "1: " << inst.getValue().operand1.value() << endl;
+    }
+    if (inst.getValue().operand2)
+    {
+
+        cout << "2: " << inst.getValue().operand2.value() << endl;
+    }
+    if (inst.getValue().operand3)
+    {
+
+        cout << "3: " << inst.getValue().operand3.value() << endl;
     }
 
     return 0;
