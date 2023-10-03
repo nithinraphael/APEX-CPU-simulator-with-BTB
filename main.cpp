d30bdb25a4a79d9fe8fab4cd4c9da848198435b9
Nithin Raphael <nithin.raphael96@gmail.com>
2023-10-03 16:15:17 -0400

    feat(main): add code to test features

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include "core/index.hpp"
+#include "core/register.hpp"
 #include "errors/index.hpp"
 #include "fs/index.hpp"
 #include "instruction/index.hpp"
 #include "instruction/instruction.hpp"
+#include "parser/index.hpp"
+#include "parser/parser.hpp"
 #include "utils/index.hpp"
 
 #include <iostream>
@@ -34,7 +38,7 @@ int main(int argc, char* argv[])
         }
     }
 
-    Result<Instruction, Error> inst = parseInstruction("SUB R3, R5");
+    Result<InstructionStr, Error> inst = parseInstructionStr("SUB R3, R5");
 
     cout << "Opcode: " << inst.getValue().opcode << endl;
     if (inst.getValue().operand1)
@@ -52,6 +56,29 @@ int main(int argc, char* argv[])
 
         cout << "3: " << inst.getValue().operand3.value() << endl;
     }
+
+    Result<InstructionStr, Error> inst2 = parseInstructionStr("SUB R3, R5");
+
+    if (inst2.isOk())
+    {
+        auto op = parser::parseOpcode(inst2.getValue().opcode);
+        if (op.isOk())
+        {
+            parser::printOpcode(op.getValue());
+        }
+    }
+
+    RegisterFile& regFile = RegisterFile::instance();
+    optional<Register*> result = regFile.get(0);
+
+    if (result)
+    {
+        Register r = *result.value();
+        r.set(8);
+        r.print();
+    }
+
+    result = regFile.get(2);
 
     return 0;
 }
