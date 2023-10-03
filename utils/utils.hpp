abeea8522b47646fd2db2dcac145eb7dcc3377db
Nithin Raphael <nithin.raphael96@gmail.com>
2023-10-03 16:10:52 -0400

    feat(utils): add string utilities

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "../errors/index.hpp"
+#include <__algorithm/remove_if.h>
+#include <algorithm>
+#include <cctype>
+#include <locale>
 #include <regex>
 #include <sstream>
+#include <string>
 #include <vector>
 
 using namespace errors;
@@ -53,6 +58,20 @@ inline string replaceCommasWith(const string& input, const string& with)
 {
     regex pattern(",+");
     return regex_replace(input, pattern, with);
+}
+
+inline string removeSpaces(const string& str)
+{
+    string s = str;
+    s.erase(remove_if(s.begin(), s.end(), ::isspace), s.end());
+    return s;
+}
+
+inline string toUpperCase(const string& s)
+{
+    string str = s;
+    transform(str.begin(), str.end(), str.begin(), ::toupper);
+    return str;
 }
 
 } // namespace utils
